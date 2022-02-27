467ad3b76929de1aa39c70828af40bad146e0cf6
Taylor Caton <taylorcaton@gmail.com>
2022-02-27 15:54:35 -0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Portfolio />
+</template>
+
+<script>
+import Portfolio from './components/Portfolio.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Portfolio,
+  },
+};
+</script>
+
+<style src="../node_modules/bulma/css/bulma.min.css"></style>
+
+<style lang="scss">
+#app {
+  margin-top: 60px;
+}
+
+html,
+body {
+  background: #ececec;
+}
+</style>
---
+++ src/components/Portfolio.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="container">
+    <h1 class="title">Taylor Caton</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'PortfolioContainer',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.container {
+  max-width: 600px !important;
+  margin: 20px auto 0 !important;
+}
+
+h1 {
+  text-rendering: optimizeLegibility;
+  color: #131313;
+  font-size: 28px;
+}
+</style>
