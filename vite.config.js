0e2d140de547b6dfe70826c342af8c4f13fdede5
Taylor Caton <taylorcaton@gmail.com>
2026-08-01 21:32:10 -0400

    Add vite-plugin-sitemap for improved SEO and site indexing

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Sitemap({
+      hostname: 'https://taylorcaton.com',
+    }),
     vueDevTools(),
   ],
   resolve: {
